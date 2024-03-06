707128bf7533da5e2f1e310d56f722ee46889bb6
川畑友亮 <engineerkoala@gmail.com>
2024-03-06 18:03:16 +0900

    add: create custom user model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,46 @@
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    # 通常のユーザーを作成するメソッド
+    def create_user(self, email, password=None):
+        # メールアドレスが提供されていない場合はエラーをスロー
+        if not email:
+            raise ValueError('The Email field must be set')
+
+        # modelメソッドを使用してユーザーオブジェクトを作成
+        # この時、normalize_emailメソッドを使用してメールアドレスを正規化
+        user = self.model(email=self.normalize_email(email))
+        # set_passwordメソッドを使用することで、パスワードをハッシュ化して設定
+        user.set_password(password)
+        # saveメソッドの引数にusing=self._dbを指定することで、デフォルトのデータベースを使用
+        user.save(using=self._db)
+
+        return user
+
+    # スーパーユーザー（管理者）を作成するメソッド
+    def create_superuser(self, email, password):
+        # 通常のユーザー作成メソッドを使用してユーザーを作成
+        user = self.create_user(email, password=password)
+        # 管理者権限を与えるためにフラグを設定
+        user.is_staff = True
+        user.is_superuser = True
+        # 変更をデータベースに保存
+        user.save(using=self._db)
+
+        return user
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=50, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # カスタムユーザーマネージャーを使用
+    objects = UserManager()
+
+    # ユーザー名として使用するフィールド
+    USERNAME_FIELD = 'email'
+
+    # ユーザーオブジェクトを文字列で表現した際の挙動を定義
+    def __str__(self):
+        return self.email
